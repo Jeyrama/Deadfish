076220c61d6df044d74bb5b54ceb46d3cc9aad62
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-30 09:04:50 -0700

    Update Deadfish.js

--- Deadfish.js
+++ Deadfish.js
@@ -34,3 +34,26 @@ function parse(data) {
 }
 
 // or
+
+const parse = data => {
+  const Commands = {
+      INCREMENT:  'i',
+      DECREMENT:  'd',
+      SQUARE:     's',
+      OUTPUT:     'o'
+  }
+
+  let outputs = [],
+      value = 0;
+  
+  data.split('').forEach(command => {
+    switch(command) {
+      case Commands.INCREMENT:  value++;                   break;
+      case Commands.DECREMENT:  value--;                   break;
+      case Commands.SQUARE:     value = Math.pow(value, 2);break;
+      case Commands.OUTPUT:     outputs.push(value);       break;
+    }
+  });
+  
+  return outputs;
+}
